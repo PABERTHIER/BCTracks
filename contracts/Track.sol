420b4aa71db5cade34890cf4e96407a2ee2f0cb5
Félicien Putinier <fel.putinier@gmail.com>
2020-09-10 17:37:50 +0200

    Add sent feature

--- contracts/Track.sol
+++ contracts/Track.sol
@@ -17,31 +17,63 @@ using SafeMath for uint256;
         uint lots_number;
         string product_name;
         uint product_number;
+        string state;
     }
 
     // Store Products
     // Fetch Product
     mapping(uint => Product) public products;
 
-    uint public LotId;
-    uint public lots_number;
+    uint LotId;
+    string state;
 
     // voted event
     event addedEvent ( uint indexed _lotId);
 
     function addProduct (string  memory _supplier_key, uint _lot_id, uint  _lots_number, string  memory _product_name, uint _product_number ) public onlyOwner {
         LotId ++;
-        products[LotId] = Product(_supplier_key,LotId, _lot_id, _lots_number, _product_name, _product_number);
+        state = "Available";
+        products[LotId] = Product(_supplier_key,LotId, _lot_id, _lots_number, _product_name, _product_number, state);
     }
 
     function Sent (uint Id, uint lots_to_send) public {
         // require a valid Product
-        require(Id > 0 && Id <= LotId && lots_to_send <= lots_number);
+        require(Id > 0 && Id <= LotId && lots_to_send <= products[Id].lots_number);
 
         //Change status
-        products[Id].lots_number = lots_number - lots_to_send;
-
+        
+        if (lots_to_send == products[Id].lots_number) {
+             products[Id].state = "Sent";
+        }
+        else {
+            LotId ++;
+            state = "Sent";
+            products[LotId] = Product(products[Id].supplier_key,LotId, products[Id].lot_id, lots_to_send, products[Id].product_name, products[Id].product_number, state);
+        
+            products[Id].lots_number = products[Id].lots_number - lots_to_send;
+        }
+        
         // trigger voted event
         emit addedEvent (Id);
     }
+    /*  function Validate (uint Id) public {
+        // require a valid Product
+        require(Id > 0 && Id <= LotId);
+
+        //Change status
+        
+        if (lots_to_send == products[Id].lots_number) {
+             products[Id].state = "Sent";
+        }
+        else {
+            LotId ++;
+            state = "Sent";
+            products[LotId] = Product(products[Id].supplier_key,LotId, products[Id].lot_id, lots_to_send, products[Id].product_name, products[Id].product_number, state);
+        
+            products[Id].lots_number = products[Id].lots_number - lots_to_send;
+        }
+        
+        // trigger voted event
+        emit addedEvent (Id);
+    } */
 }
