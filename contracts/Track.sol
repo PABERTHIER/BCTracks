2aee20d607b86fff1efeefac4d125415af2a0985
Gogbox <apiperaud@gmail.com>
2020-09-11 11:43:45 +0200

    Add functions purpose and accepted take over

--- contracts/Track.sol
+++ contracts/Track.sol
@@ -11,7 +11,9 @@ using SafeMath for uint256;
 
     // Model a Candidate
     struct Product {
-        string supplier_key;
+        address payable supplier_key;
+        address payable owner_key;
+        address payable [] delivery_key;
         uint256 id;
         uint lot_id;
         uint lots_number;
@@ -29,11 +31,14 @@ using SafeMath for uint256;
 
     // voted event
     event addedEvent ( uint indexed _lotId);
+    event purposeEvent ( uint indexed _lotId, address payable _delivery_key);
+    event acceptedEvent ( uint indexed _lotId, address payable _delivery_key);
 
-    function addProduct (string  memory _supplier_key, uint _lot_id, uint  _lots_number, string  memory _product_name, uint _product_number ) public onlyOwner {
+    function addProduct (address payable _supplier_key, uint _lot_id, uint  _lots_number, string  memory _product_name, uint _product_number ) public onlyOwner {
         LotId ++;
         state = "Available";
-        products[LotId] = Product(_supplier_key,LotId, _lot_id, _lots_number, _product_name, _product_number, state);
+        address payable [] memory delivery_keys;
+        products[LotId] = Product(_supplier_key, _supplier_key, delivery_keys, LotId, _lot_id, _lots_number, _product_name, _product_number, state);
     }
 
     function Sent (uint Id, uint lots_to_send) public {
@@ -43,12 +48,12 @@ using SafeMath for uint256;
         //Change status
         
         if (lots_to_send == products[Id].lots_number) {
-             products[Id].state = "Sent";
+             products[Id].state = "On Preparation";
         }
         else {
             LotId ++;
-            state = "Sent";
-            products[LotId] = Product(products[Id].supplier_key,LotId, products[Id].lot_id, lots_to_send, products[Id].product_name, products[Id].product_number, state);
+            state = "On Preparation";
+            products[LotId] = Product(products[Id].supplier_key, products[Id].owner_key, products[Id].delivery_key, LotId, products[Id].lot_id, lots_to_send, products[Id].product_name, products[Id].product_number, state);
         
             products[Id].lots_number = products[Id].lots_number - lots_to_send;
         }
@@ -56,24 +61,37 @@ using SafeMath for uint256;
         // trigger voted event
         emit addedEvent (Id);
     }
-    /*  function Validate (uint Id) public {
-        // require a valid Product
-        require(Id > 0 && Id <= LotId);
 
-        //Change status
+    function Purpose_takeOver_Lot (uint _lot_Id) public {
+        // require a valid Product
+        require(_lot_Id > 0 && _lot_Id <= LotId && keccak256(abi.encodePacked(products[_lot_Id].state)) ==  keccak256("On Preparation"));
+
+        //Add a delivery purpose
+        products[_lot_Id].delivery_key.push(msg.sender);
         
-        if (lots_to_send == products[Id].lots_number) {
-             products[Id].state = "Sent";
+        // trigger purpose event
+        emit purposeEvent(_lot_Id, msg.sender);
+    }
+
+    function Accepted_takeOver_Lot (uint _lot_Id, address payable _delivery_key) public {
+        // require a valid Product
+        require(_lot_Id > 0 && _lot_Id <= LotId && keccak256(abi.encodePacked(products[_lot_Id].state)) ==  keccak256("On Preparation"));
+
+        // require a valid delivery key
+        bool is_implemented = false;
+        for(uint increment = 0; increment < products[_lot_Id].delivery_key.length; increment++){
+            if(products[_lot_Id].delivery_key[increment] == _delivery_key){
+                is_implemented = true;
+            }
         }
-        else {
-            LotId ++;
-            state = "Sent";
-            products[LotId] = Product(products[Id].supplier_key,LotId, products[Id].lot_id, lots_to_send, products[Id].product_name, products[Id].product_number, state);
+        require(is_implemented);
+
+        // update status
+        products[_lot_Id].delivery_key = [_delivery_key];
+        products[_lot_Id].state = "Sent";
         
-            products[Id].lots_number = products[Id].lots_number - lots_to_send;
-        }
-        
-        // trigger voted event
-        emit addedEvent (Id);
-    } */
+        // trigger purpose event
+        emit purposeEvent(_lot_Id, products[_lot_Id].delivery_key[0]);
+    }
+
 }
