5ff79cef7df0ba3bfc4f1bf38fdcc465c36b7e49
Gogbox <apiperaud@gmail.com>
2020-09-11 18:03:59 +0200

    Code Review + changenowner on buy

--- contracts/Track.sol
+++ contracts/Track.sol
@@ -9,14 +9,14 @@ contract Track is Ownable {
 
 using SafeMath for uint256;
 
-    // Model a Candidate
+    // Model a Bundle
     struct Product {
         address payable supplier_key;
         address payable owner_key;
         address payable delivery_key;
         uint256 id;
-        uint lot_id;
-        uint lots_number;
+        uint bundle_id;
+        uint bundles_number;
         string product_name;
         uint product_number;
         string state;
@@ -27,72 +27,74 @@ using SafeMath for uint256;
     // Fetch Product
     mapping(uint => Product) public products;
 
-    uint LotId;
-    string state;
-    string certstate;
+    uint total_bundelId;
 
+    event addedEvent ( uint indexed _bundleId);
+    event boughtEvent ( uint indexed _bundleId, address payable _new_owner);
+    event submitTakeOverEvent ( uint indexed _bundleId, address payable _delivery_key);
+    event validateTakeOverEvent ( uint indexed _bundleId, address payable _delivery_key);
 
-    // voted event
-    event addedEvent ( uint indexed _lotId);
-    event submitTakeOverEvent ( uint indexed _lotId, address payable _delivery_key);
-    event validateTakeOverEvent ( uint indexed _lotId, address payable _delivery_key);
-
-    function AddLots (uint _lot_id, uint  _lots_number, string  memory _product_name, uint _product_number ) public onlyOwner {
-        LotId ++;
-        state = "Available";
-        certstate = "Not certified yet";
-        address payable _none_delivery = 0x0000000000000000000000000000000000000000;
+    function Add_Bundle (uint _bundle_id, uint  _bundles_nulber, string  memory _product_name, uint _product_number ) public onlyOwner {
+        total_bundelId ++;
+        string memory state = "Available";
+        string memory certstate = "Not Certified";
+        address payable _none_delivery = address(0);
         address payable _supplier_key = msg.sender;
-        products[LotId] = Product(_supplier_key, _supplier_key, _none_delivery, LotId, _lot_id, _lots_number, _product_name, _product_number, state, certstate);
+        products[total_bundelId] = Product(_supplier_key, _supplier_key, _none_delivery, total_bundelId, _bundle_id, _bundles_nulber, _product_name, _product_number, state, certstate);
+        emit addedEvent (total_bundelId);
     }
-    //command_lot
-    function Sent (uint Id, uint lots_to_send) public {
+ 
+    function Buy_Bundle (uint Id, uint _bundles_to_send) public {
         // require a valid Product
-        require(Id > 0 && Id <= LotId && lots_to_send <= products[Id].lots_number);
+        require(Id > 0 && Id <= total_bundelId && _bundles_to_send <= products[Id].bundles_number);
+
         //Change status
-        if (lots_to_send == products[Id].lots_number) {
-             products[Id].state = "On Preparation";
+        if (_bundles_to_send == products[Id].bundles_number) {
+            products[Id].state = "In Process";
+            products[Id].owner_key = msg.sender;
         }
         else {
-            LotId ++;
-            state = "On Preparation";
-            products[LotId] = Product(products[Id].supplier_key, products[Id].owner_key, products[Id].delivery_key, LotId, products[Id].lot_id, lots_to_send, products[Id].product_name, products[Id].product_number, state, products[Id].certstate);
-            products[Id].lots_number = products[Id].lots_number - lots_to_send;
+            total_bundelId ++;
+            string memory state = "In Process";
+            products[total_bundelId] = Product(products[Id].supplier_key, products[Id].owner_key, products[Id].delivery_key, total_bundelId, products[Id].bundle_id, _bundles_to_send, products[Id].product_name, products[Id].product_number, state, products[Id].certstate);
+            products[Id].bundles_number = products[Id].bundles_number - _bundles_to_send;
+            products[Id].owner_key = msg.sender;
         }
-        // trigger voted event
-        emit addedEvent (Id);
+
+        // trigger bought event
+        emit boughtEvent (Id, products[Id].owner_key);
     }
-    function Submit_takeOver_Lot (uint _lot_Id, address payable _delivery_key) public {
+    function Submit_takeOver_Bundle (uint _bundle_id, address payable _delivery_key) public {
         // require a valid Product
-        require(_lot_Id > 0 && _lot_Id <= LotId && keccak256(abi.encodePacked(products[_lot_Id].state)) ==  keccak256("On Preparation"));
+        require(_bundle_id > 0 && _bundle_id <= total_bundelId && keccak256(abi.encodePacked(products[_bundle_id].state)) ==  keccak256("In Process"));
 
         //Add a delivery purpose
-        products[_lot_Id].delivery_key = _delivery_key;
+        products[_bundle_id].delivery_key = _delivery_key;
         
         // trigger purpose event
-        emit submitTakeOverEvent(_lot_Id, _delivery_key);
+        emit submitTakeOverEvent(_bundle_id, _delivery_key);
     }
 
-    function Accepted_takeOver_Lot (uint _lot_Id) public {
+    function Accepted_takeOver_Bundle (uint _bundle_id) public {
         // require a valid Product
-        require(_lot_Id > 0 && _lot_Id <= LotId && keccak256(abi.encodePacked(products[_lot_Id].state)) ==  keccak256("On Preparation"));
+        require(_bundle_id > 0 && _bundle_id <= total_bundelId && keccak256(abi.encodePacked(products[_bundle_id].state)) ==  keccak256("In Process"));
 
         // require a valid Sender
-        require(msg.sender == products[_lot_Id].delivery_key); 
+        require(msg.sender == products[_bundle_id].delivery_key); 
 
         // update status
-        products[_lot_Id].state = "Sent";
+        products[_bundle_id].state = "Sent";
         
         // trigger purpose event
-        emit validateTakeOverEvent(_lot_Id, products[_lot_Id].delivery_key);
+        emit validateTakeOverEvent(_bundle_id, products[_bundle_id].delivery_key);
     }
     function Certify_Lots (uint Id, string memory state0) public {
         // require a valid Product
-        require(Id > 0 && Id <= LotId && keccak256(abi.encodePacked(products[Id].certstate)) != keccak256("Unsalable" ));
+        require(Id > 0 && Id <= total_bundelId && keccak256(abi.encodePacked(products[Id].certstate)) != keccak256("Unsalable" ));
 
         //Change status
-        certstate = state0;
-        products[LotId] = Product(products[Id].supplier_key, products[Id].owner_key, products[Id].delivery_key, products[Id].id, products[Id].lot_id, products[Id].lots_number, products[Id].product_name, products[Id].product_number, products[Id].state, certstate);
+        string memory certstate = state0;
+        products[total_bundelId] = Product(products[Id].supplier_key, products[Id].owner_key, products[Id].delivery_key, products[Id].id, products[Id].bundle_id, products[Id].bundles_number, products[Id].product_name, products[Id].product_number, products[Id].state, certstate);
         
         // trigger voted event
         emit addedEvent (Id);
