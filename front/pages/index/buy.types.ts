34c6465fe235a504ad7b5c49976129a938ae280e
PABERTHIER <39381248+PABERTHIER@users.noreply.github.com>
2020-09-22 15:20:37 +0200

    Correction type

--- front/pages/index/buy.types.ts
+++ front/pages/index/buy.types.ts
@@ -5,11 +5,20 @@ export interface D {
 }
 
 export interface M {
+  getContractInstance?: () => void
   getBundle: () => void
   buyBundle: () => void
 }
 
 export interface C {
+  web3?: {
+    isInjected: boolean
+    web3Instance: any
+    networkId: number
+    coinbase: string
+    balance: number
+    error: any
+  }
   contractInstance?: any
   hasBundleAvailable: boolean
   hasEnoughAmount: boolean
